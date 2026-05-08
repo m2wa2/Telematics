8791946e853c6edec641ab0066d0ac3e92251b74
m2wa2 <jipeshughuli@gmail.com>
2026-05-08 10:56:45 +0300

    Add bookings API route implementation

---
+++ .next/server/app/api/bookings/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/bookings/route.js")
+R.c("server/chunks/src_app_api_bookings_route_ts_0-u96dh._.js")
+R.c("server/chunks/[root-of-the-server]__00h.kft._.js")
+R.c("server/chunks/[root-of-the-server]__0bozk3p._.js")
+R.c("server/chunks/_next-internal_server_app_api_bookings_route_actions_10f.xiz.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/bookings/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/bookings/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
