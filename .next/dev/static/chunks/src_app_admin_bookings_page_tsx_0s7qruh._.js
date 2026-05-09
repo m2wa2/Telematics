b35119c1407bbe6586fa66f848fa5a005e9370aa
m2wa2 <jipeshughuli@gmail.com>
2026-05-09 05:25:48 +0300

    Delete .next directory

--- .next/dev/static/chunks/src_app_admin_bookings_page_tsx_0s7qruh._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_admin_bookings_page_tsx_0i~7pqy._.js"],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_0pdltnm._.css","static/chunks/node_modules_next_dist_0z8ikg~._.js","static/chunks/src_components_0_s6pe9._.js"],
-    source: "dynamic"
-});
