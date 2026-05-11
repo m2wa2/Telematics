e3a6586e3db23879ff82f08505379dd3bd96f15e
m2wa2 <jipeshughuli@gmail.com>
2026-05-11 04:09:51 +0300

    Create route.js

---
+++ next/server/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/[...nextauth]/route.js")
+R.c("server/chunks/[root-of-the-server]__0jl0eb9._.js")
+R.c("server/chunks/[root-of-the-server]__0sdgevn._.js")
+R.c("server/chunks/[root-of-the-server]__0p3fo6l._.js")
+R.c("server/chunks/_next-internal_server_app_api_auth_[___nextauth]_route_actions_04_0y9h.js")
+R.m(3413)
+module.exports=R.m(3413).exports
