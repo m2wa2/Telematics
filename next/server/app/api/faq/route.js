1f91cfd6bfaf206bc514ff275fd0e382c664a1e6
m2wa2 <jipeshughuli@gmail.com>
2026-05-11 04:44:23 +0300

    Add route.js for FAQ API endpoint

---
+++ next/server/app/api/faq/route.js
@@ -0,0 +1,8 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/faq/route.js")
+R.c("server/chunks/[root-of-the-server]__0h06wa.._.js")
+R.c("server/chunks/[root-of-the-server]__0p3fo6l._.js")
+R.c("server/chunks/node_modules_next_11synfn._.js")
+R.c("server/chunks/_next-internal_server_app_api_faq_route_actions_08xzyxq.js")
+R.m(48088)
+module.exports=R.m(48088).exports
+
